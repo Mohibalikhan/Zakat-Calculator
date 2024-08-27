703bceefbbcec2ea29f39d957cd95cf2748cd24a
Mohibalikhan <khanmohibali1@gmail.com>
2024-08-27 22:20:22 +0500

    Zakat Calculator

---
+++ index.ts
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+let mywealth = await inquirer.prompt([{
+    name: "money",
+    type: "number",
+    message: "Enter your Total wealth to check How much Zakat you have to be Paid:"
+    }])
+   let zakat:number = 0.025 ;
+   console.log(`Your Zakat is:${zakat*mywealth.money}`);
+   console.log("Thanks for Using Our Services...");
